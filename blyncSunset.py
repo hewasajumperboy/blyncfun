0772226200f825e435e93ab48ed92de8a733931e
hewasajumperboy <brandon@collier.in>
2022-11-06 16:56:18 -0800

    remove example code comment

--- blyncSunset.py
+++ blyncSunset.py
@@ -34,28 +34,3 @@ while True:
 
     sleep(10)
 
-'''
-from astral import LocationInfo
-from astral.sun import sun
-from datetime import datetime
-from time import sleep
-import pytz
-
-utc=pytz.UTC
-
-city = LocationInfo("London", "England", "Europe/London", 51.5, -0.116) # swap with your location
-
-while True:
-
-    s = sun(city.observer)
-    time_now = utc.localize(datetime.now())
-    sunrise = s["sunrise"]
-    dusk = s["dusk"]
-
-    if time_now > sunrise and time_now < dusk: # am I in between sunset and dusk?
-        print("Daylight")
-    else:
-        print("Switch on light")
-
-    sleep(60)
-'''
