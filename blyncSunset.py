87b662e7bdd14efd527f8142b37da5515202b8e3
hewasajumperboy <brandon@collier.in>
2022-11-12 11:22:53 -0800

    refactor to blink yellow when lullaby is upcoming

--- blyncSunset.py
+++ blyncSunset.py
@@ -1,36 +1,63 @@
 from astral import LocationInfo
 from astral.sun import sun
-import pytz
+#import pytz
 from datetime import datetime
 from datetime import time
 from zoneinfo import ZoneInfo
 from time import sleep
 import os
+import subprocess
+import signal
 
 localCity="Bend"
 localRegion="Oregon"
 localTzName="America/Los_Angeles"
 localZoneInfo = ZoneInfo(localTzName)
-localLatitude = 44.074278 
+localLatitude = 44.074278
 localLongitude = -121.270139
+localElevation = 1104.29
 localLocationInfo = LocationInfo(localCity, localRegion, localTzName, localLatitude, localLongitude)
-
-#print("Timezone: ", localTzName)
-#print("sunset time: ", localSun["sunset"])
-#print("dusk time: ", localSun["dusk"])
-#print("sunrise time: ", localSun["sunrise"])
-#print("current time: ", datetime.now(tz=localZoneInfo))
+dtNow = datetime.now(tz=localZoneInfo)
+localSun = sun(observer = localLocationInfo.observer, tzinfo=localZoneInfo)
+blinkProcess = subprocess.Popen('busylight --all blink 0xffff00', shell = True, stdout=subprocess.PIPE, preexec_fn=os.setsid)
+os.killpg(os.getpgid(blinkProcess.pid), signal.SIGTERM)
+bedtime = datetime.combine(dtNow.date(), time(21, 30, 0, 0, tzinfo=localZoneInfo))
+bedtimeWarnStart = datetime.combine(dtNow.date(), time(19, 52, 0, 0, tzinfo=localZoneInfo))
+bedtimeWarnEnd = datetime.combine(dtNow.date(), time(19, 55, 0, 0, tzinfo=localZoneInfo))
+blinkProcessRunning = False
 
 while True:
-    localSun = sun(observer = localLocationInfo.observer, tzinfo=localZoneInfo)
+    #if new day?
+    if dtNow.day != datetime.now().day:
+        #update day variable with today
+        dtNow = datetime.now(tz=localZoneInfo)
+        #recalculate the sun today
+        localSun = sun(observer = localLocationInfo.observer, tzinfo=localZoneInfo)
+        #recalculate bedtime today
+        bedtime = datetime.combine(dtNow.date(), time(21, 30, 0, 0, tzinfo=localZoneInfo))
+        bedtimeWarnStart = datetime.combine(dtNow.date(), time(19, 52, 0, 0, tzinfo=localZoneInfo))
+        bedtimeWarnEnd = datetime.combine(dtNow.date(), time(19, 55, 0, 0, tzinfo=localZoneInfo))
+    else:
+        dtNow = datetime.now(tz=localZoneInfo)
 
-    dtNow = datetime.now(tz=localZoneInfo)
-    bedtime = datetime.combine(dtNow.date(), time(20, 0, 0, 0, tzinfo=localZoneInfo))
-
-    if dtNow > localSun["dusk"] and dtNow < bedtime: # am I in between sunset and bedtime?
+    # check if we're between dusk and bedtime (note: this might be an issue in the summer)
+    if dtNow > localSun["dusk"] and dtNow < bedtime:
         os.system('busylight --all on 0xbbffff')
     else:
         os.system('busylight --all off')
 
-    sleep(120)
+    #check if we should start warning  of impending bedtime
+    if dtNow >= bedtimeWarnStart and dtNow <= bedtimeWarnEnd:
+        if blinkProcessRunning == False:
+            blinkProcess = subprocess.Popen('busylight --all blink 0xffff00', shell = True, stdout=subprocess.PIPE, preexec_fn=os.setsid)
+            blinkProcessRunning = True
+    else:
+        #send signal to kill process group (will kill all child processes started by shell)
+        if blinkProcessRunning == True:
+            os.killpg(os.getpgid(blinkProcess.pid), signal.SIGTERM)
+            blinkProcessRunning = False
 
+    sleep(60)
+
+#if/when you calculate effects of elevation... or if you care...
+#Location(localLocationInfo).dusk(observer_elevation = 1104.29)
